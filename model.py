66179486436114d8466361b0fbdfb600841a3b96
Yves Velasquez <vyves@csu.fullerton.edu>
2025-11-05 13:51:41 -0800

    Initial setup

--- model.py
+++ model.py
@@ -1,4 +1,12 @@
 from datasets import load_dataset
 import pandas as pd
 
+# Load Train and split
+from datasets import load_dataset
 
+data_files={"train": "train_clean.parquet","test": "test_clean.parquet"}
+
+dataset = load_dataset("HallowsYves/CPSC483-data", data_files=data_files)
+
+train_df = dataset["train"].to_pandas()
+test_df = dataset["test"].to_pandas()
